1dad8c0cd66148972ff0b1443e4e5e5bd16c8010
Andrew Glines <aglines@gmail.com>
2017-09-18 15:44:19 -0700

    create stupid React code that will be superseded by JSX code theyll tell me about in the following paragraph

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-var greeting = React.createElement('h1', {}, 'Hello world');
-var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
-var app = React.createElement('div', {}, greeting, clock);
+
+
+var heading = React.createElement('h1', {}, 'help queue');
+var ticketLocation = React.createElement('h3', {}, '3a');
+var ticketNames = React.createElement('h3', {}, 'Xhayleigh and Thanatosia');
+var ticketIssue = React.createElement('h3', {}, 'Firebase won\'t save record');
+
+var app = React.createElement('div', {}, heading, ticketLocation, ticketNames, ticketIssue);
 
 ReactDOM.render(
   app,
